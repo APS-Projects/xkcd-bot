cd4cb2251305118e524932772056de2d7f4d1f37
sdana <seth.dana@gmail.com>
2019-02-17 11:23:10 -0600

    Initial commit. Somewhat working, trying to figure out why my fetch is returning a readable stream as the body isntead of json

---
+++ app/routes.py
@@ -0,0 +1,37 @@
+from app import app
+from flask import jsonify
+import re
+from bs4 import BeautifulSoup
+import requests
+
+@app.route("/getcomic/<string:search_query>", methods=['GET'])
+def get_comic(search_query):
+    # search_query = input("xkcd to search for? ")
+
+    url = "https://duckduckgo.com/html/?q=site%3Axkcd.com%20" + search_query
+
+    data = requests.get(url)
+
+
+    #Local HTML file for testing purposes
+    # filename = './nothing1.html'
+
+    #Initialize our soup object from our HTML results (or local file)
+    # soup = BeautifulSoup(open(filename), 'html.parser')
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    #Use BeautifulSoup to parse through the entire HTML and return the specific anchor tags we're looking for
+    result_urls = soup.find_all('a', {'class': 'result__url'})
+
+    #Results from our regex matches
+    regex_results = []
+
+    #Loop through all the anchor tags we captured and apply some regex to get the text we want, then push that to our results list
+    for result in result_urls:
+        x = re.findall("xkcd.com/\d+/", str(result))
+        print(regex_results)
+        #Check if regex matched
+        if x and x[0] not in regex_results:
+            regex_results.append(x[0])
+    response = jsonify(regex_results)
+    return response
